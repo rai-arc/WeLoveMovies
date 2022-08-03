6d8712fd32912c037db61337b4a410a1fcb60e5c
rai-arc <raiarcebido@gmail.com>
2022-08-03 11:35:32 -0400

    added cors to all routers

--- src/theaters/theaters.router.js
+++ src/theaters/theaters.router.js
@@ -1,6 +1,9 @@
 const router = require("express").Router({ mergeParams: true });
 const controller = require("./theaters.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
+
+router.use(cors())
 
 router.route("/")
   .get(controller.list)
